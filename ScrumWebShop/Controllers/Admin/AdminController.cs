07d1d30dd72160419d72e5a4680ebac13ef088ef
Erika Fröjd <erika.frojd1@learnet.se>
2020-08-24 08:39:07 +0200

    Editering fungerar och product + categories är borta

--- ScrumWebShop/Controllers/Admin/AdminController.cs
+++ ScrumWebShop/Controllers/Admin/AdminController.cs
@@ -92,9 +92,64 @@ namespace ScrumWebShop.Controllers.Admin
         }
 
 
+        [HttpGet]
+        public async Task<IActionResult> EditUser(string id)
+        {
+            var userExists = await _userManager.FindByIdAsync(id);
+
+            if (userExists == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id:  {id} cannot be found";
+                return RedirectToAction("Users");
+            }
+
+            var model = new CustomerViewModel()
+            {
+                Id = userExists.Id,
+                FirstName = userExists.FirstName,
+                LastName = userExists.LastName,
+                StreetAddress = userExists.StreetAdress,
+                City = userExists.City,
+                PostalCode = userExists.PostalCode,
+                Country = userExists.Country,
+                PhoneNumber = userExists.PhoneNumber,
+                Email = userExists.Email
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(CustomerViewModel model)
+        {
+            var userExists = await _userManager.FindByIdAsync(model.Id);
+
+            if (userExists == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id:  {model.Id} cannot be found";
+                return View(model);
+            }
 
 
+            userExists.PhoneNumber = model.PhoneNumber;
+            userExists.FirstName = model.FirstName;
+            userExists.LastName = model.LastName;
+            userExists.StreetAdress = model.StreetAddress;
+            userExists.PostalCode = model.PostalCode;
+            userExists.City = model.City;
+            userExists.Email = model.Email;
+            var result = await _userManager.UpdateAsync(userExists);
 
+            if (result.Succeeded)
+                return RedirectToAction("Users");
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
 
 
     }
