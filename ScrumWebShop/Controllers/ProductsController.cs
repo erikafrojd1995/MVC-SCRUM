1e8bf1ba0997471ecf26d966de5358fc56129793
Erika Fröjd <erika.frojd1@learnet.se>
2020-08-27 11:16:47 +0200

    authorization fixat så att customer och utloggad ej kan nå crud på produkterna

--- ScrumWebShop/Controllers/ProductsController.cs
+++ ScrumWebShop/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using ScrumWebShop.Data;
 
 namespace ScrumWebShop.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -18,7 +20,7 @@ namespace ScrumWebShop.Controllers
             _context = context;
         }
 
-
+        [AllowAnonymous]
         //GET: Products + SEARCH BY KEYWORD in ProductName or ProductDescription
         public IActionResult Index(string searchString)
         {
@@ -31,7 +33,7 @@ namespace ScrumWebShop.Controllers
             return View("Index", products);
         }
 
-
+        [AllowAnonymous]
         // GET: Products/Details/5
         public async Task<IActionResult> Details(int? id)
         {
